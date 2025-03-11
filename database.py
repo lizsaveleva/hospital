2b770a392c54ef8ce46a1c74383a573d4635a1e6
lizsaveleva <elizaveta.5avelieva@yandex.com>
2025-03-11 21:56:50 +0300

    Add files via upload

---
+++ database.py
@@ -0,0 +1,23 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            password="",
+            database="hospital_bd"
+        )
+        if connection.is_connected():
+            print("1.Соединение с базой данных успешно установлено")
+        return connection
+    except Error as e:
+        print(f"1.Ошибка подключения: {e}")
+        return None
+
+def close_connection(connection):
+    if connection and connection.is_connected():
+        connection.close()
+        print("3.Соединение с базой данных закрыто")
