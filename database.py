69971fdd204d279e4c04788dda0013f6855b6caa
lizsaveleva <elizaveta.5avelieva@yandex.com>
2025-03-19 10:11:44 +0300

    Update database.py

--- database.py
+++ database.py
@@ -1,22 +1,24 @@
 import mysql.connector
 from mysql.connector import Error
 
-try:
-    conn = mysql.connector.connect(
-        host="127.0.0.1",
-        port=3306,
-        user="root",
-        password="root",
-        database="hospital"
-    )
+def create_connection():
+    try:
+        connection = mysql.connector.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            password="root",
+            database="hospital"
+        )
+        if connection.is_connected():
+            print("1.Соединение с базой данных успешно установлено")
+        return connection
+    except Error as e:
+        print(f"1.Ошибка подключения: {e}")
+        return None
 
-    if conn.is_connected():
-        print("1.Соединение с базой данных успешно установлено")
-
-except Error as e:
-    print(f"1.Ошибка подключения: {e}")
-
-finally:
-    if conn and conn.is_connected():
-        conn.close()
+def close_connection(connection):
+    if connection and connection.is_connected():
+        connection.close()
         print("3.Соединение с базой данных закрыто")
+
